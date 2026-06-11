5bd42094e5edf95acf63ce9546ec0d6f8e6d81cd
jukogo <jukogo@gmail.com>
2026-06-11 17:55:15 +0200

    Configurar Astro y GitHub Pages para portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://jukogo.github.io/portfolio-fotos",
+  base: "/portfolio-fotos",
+  output: "static",		
+});
